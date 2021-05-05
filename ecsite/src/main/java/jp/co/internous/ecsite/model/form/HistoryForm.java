0428d239ea61c6b6e8b58c633ff4058df490e988
stk9805 <biberia009@gmail.com>
2021-05-06 04:56:21 +0900

    ECサイト

---
+++ ecsite/src/main/java/jp/co/internous/ecsite/model/form/HistoryForm.java
@@ -0,0 +1,17 @@
+package jp.co.internous.ecsite.model.form;
+
+import java.io.Serializable;
+
+public class HistoryForm implements Serializable {
+	private static final long serialVersionUTD = 1L;
+	
+	private String userId;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+}
